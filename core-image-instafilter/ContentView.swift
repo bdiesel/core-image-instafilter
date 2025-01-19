eeaa2a4e32157f83277d1508ce2be021e139fa48
Brian Diesel <bri@ndiesel.net>
2025-01-19 13:14:14 -0500

    Initial Commit

---
+++ core-image-instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  core-image-instafilter
+//
+//  Created by Brian Diesel on 1/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ core-image-instafilter/core_image_instafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  core_image_instafilterApp.swift
+//  core-image-instafilter
+//
+//  Created by Brian Diesel on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct core_image_instafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
