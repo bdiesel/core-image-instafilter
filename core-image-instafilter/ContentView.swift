be69a2ec77ff4b92d393edbc9d1f6d37cfad4a44
Brian Diesel <bri@ndiesel.net>
2025-01-20 15:00:35 -0500

    Importing image into SwiftUI using PhotosPicker

--- core-image-instafilter/ContentView.swift
+++ core-image-instafilter/ContentView.swift
@@ -5,13 +5,13 @@
 //  Created by Brian Diesel on 1/19/25.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var proceesedImage: Image?
     @State private var filterIntensity = 0.5
-    
-    
+    @State private var selectedItem: PhotosPickerItem?
     
     
     var body: some View {
@@ -19,16 +19,20 @@ struct ContentView: View {
             VStack {
                 Spacer()
                 
-                if let proceesedImage {
-                    proceesedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus",
-                                           description: Text("Tap to import a photo")
-                    )
+                PhotosPicker(selection: $selectedItem) {
+                    if let proceesedImage {
+                        proceesedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus",
+                                               description: Text("Tap to import a photo")
+                        )
+                    }
                 }
-                
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+    
                 Spacer()
                 
                 HStack {
@@ -50,6 +54,14 @@ struct ContentView: View {
     
     func changeFilter(){
         
+    }
+    
+    func loadImage(){
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type:
+                Data.self) else {return}
+            guard let inputImage = UIImage(data: imageData) else { return }
+        }
     }
 }
 
