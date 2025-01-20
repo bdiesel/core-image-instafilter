3e33a9e2fd39f4ac91584442c5e0451f9a80bc1a
Brian Diesel <bri@ndiesel.net>
2025-01-20 15:29:24 -0500

    Basic image filtering using core image

--- core-image-instafilter/ContentView.swift
+++ core-image-instafilter/ContentView.swift
@@ -4,15 +4,18 @@
 //
 //  Created by Brian Diesel on 1/19/25.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var proceesedImage: Image?
+    @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     @State private var selectedItem: PhotosPickerItem?
     
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
         NavigationStack{
@@ -20,8 +23,8 @@ struct ContentView: View {
                 Spacer()
                 
                 PhotosPicker(selection: $selectedItem) {
-                    if let proceesedImage {
-                        proceesedImage
+                    if let processedImage {
+                        processedImage
                             .resizable()
                             .scaledToFit()
                     } else {
@@ -38,6 +41,7 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
                 
@@ -61,8 +65,23 @@ struct ContentView: View {
             guard let imageData = try await selectedItem?.loadTransferable(type:
                 Data.self) else {return}
             guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
     }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
 }
 
 #Preview {
